d5dd8c327d408a4db1829c01f31d36cd1339e4cb
Daniel Mills <danielmillst@gmail.com>
2021-03-02 05:18:47 -0500

    tests

--- src/main/java/art/arcane/quill/service/TestService.java
+++ src/main/java/art/arcane/quill/service/TestService.java
@@ -1,6 +1,7 @@
 package art.arcane.quill.service;
 
 import art.arcane.quill.Quill;
+import art.arcane.quill.collections.KList;
 import art.arcane.quill.logging.L;
 
 public class TestService extends QuillService
@@ -13,13 +14,25 @@ public class TestService extends QuillService
     @ServiceWorker
     private TestServiceWorker testServiceWorker;
 
+    @ServiceWorker
+    private ConsoleServiceWorker console;
+
     public TestService() {
         super("TestSVC");
     }
 
     @Override
     public void onEnable() {
-        L.v("Enabled!");
+        console.registerCommand("foo", params -> {
+            L.v("Bar!");
+            // TRUE = handled, FALSE = try another command (for duplicate names)
+            return true;
+        });
+
+        console.registerCommand("echo", params -> {
+            L.v(KList.from(params).toString(" "));
+            return true;
+        });
     }
 
     @Override
