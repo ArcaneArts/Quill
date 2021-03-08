a67a06a124dc29c603a9f247a9efc33651923293
Daniel Mills <danielmillst@gmail.com>
2021-03-08 17:20:58 -0500

    v

--- src/main/java/art/arcane/quill/collections/KSet.java
+++ src/main/java/art/arcane/quill/collections/KSet.java
@@ -2,6 +2,7 @@ package art.arcane.quill.collections;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class KSet<T> extends HashSet<T>
 {
@@ -25,5 +26,28 @@ public class KSet<T> extends HashSet<T>
 	public KSet(int initialCapacity)
 	{
 		super(initialCapacity);
+	}
+
+	public String toString(String split)
+	{
+		if (isEmpty())
+		{
+			return "";
+		}
+
+		if (size() == 1)
+		{
+			return iterator().next().toString();
+		}
+
+		StringBuilder b = new StringBuilder();
+		Iterator<T> g = iterator();
+
+		while (g.hasNext())
+		{
+			b.append(split + g.next());
+		}
+
+		return b.toString().substring(split.length());
 	}
 }
